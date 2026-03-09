ccda5fa390494847f7c1c6f9a12d15f9413b857c
laurindolucas <caiolucax2024@gmail.com>
2026-03-09 16:54:58 -0300

    ajustando css da atividade 02

--- atividade02-pfe/src/app/components/profile.jsx
+++ atividade02-pfe/src/app/components/profile.jsx
@@ -1,9 +1,10 @@
+
 export default function Profile() {
     return (
         <div className="container">
-            <img src="@/atividade02/public/image.jpeg" alt="" className="image-profile"/>
+            <img src="./image/image.jpeg" alt="" className="image-profile"/>
             <h1 className="name-profile">Caio Laurindo</h1>
-            <p className="description-profile"></p>
+            <p className="description-profile">Estudante de Sistemas para Internet na Universidade Católica de Pernambuco, com foco em desenvolvimento web e monitor no Porto digital.</p>
         </div>
 
     )
--- atividade02-pfe/src/app/page.js
+++ atividade02-pfe/src/app/page.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
 import Profile from "./components/profile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="title">Site criado para a disciplina Programação Front-End</h1>
-      <Profile/>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-blue-500 text-white font-sans text-center">
+      <h1 className="title">
+        Site criado para a disciplina Programação Front-End
+      </h1>
+
+      <Profile />
     </div>
   );
 }
